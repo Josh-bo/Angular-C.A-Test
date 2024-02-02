5a3f7461b3ef9760e6bab0b635c2387c24127d25
Bamidele joshua olayinka <Olayinka775@gmail.com>
2024-02-02 14:08:43 +0100

    First C.A test

--- src/app/guards/admin-guard.guard.ts
+++ src/app/guards/admin-guard.guard.ts
@@ -6,18 +6,18 @@ export const adminGuardGuard: CanActivateFn = (route, state) => {
 
   if (localStorage['CurrentUser']) {
     const getAdmin = JSON.parse(localStorage['CurrentUser']);
-  
-    if (Array.isArray(getAdmin)) {
-      let newStudents = getAdmin.find((select : any, i : number) => select.option == "Admin");
-      console.log(newStudents);
-    } else {
-      console.error("CurrentUser is not an array");
-      routing.navigate(['/signIn'])
+      if (Array.isArray(getAdmin) || getAdmin != null) {
+        let newStudents = getAdmin.find((select : any, i : number) => select.option === "Admin");
+        console.log(newStudents);
+      } else {
+        console.error("CurrentUser is not an array");
+        routing.navigate(['/signIn'])
+      }
     }
-  } else {
-    console.error("CurrentUser not found in localStorage");
-    routing.navigate(['/signIn'])
-  }
+  else {
+   console.error("CurrentUser not found in localStorage");
+   routing.navigate(['/signIn'])
+ }
   
 
   return true;
